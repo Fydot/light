ea1a9dcf73a407a9c57b46597b94c66be821617e
hdd <hdd@zhihu.com>
2014-04-16 14:49:22 +0800

    Initial Work

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+
+setup(name='light',
+      version='0.0.19',
+      author='huangdiandian',
+      author_email='ifidot@gmail.com',
+      description='light is a light web server',
+      license='MIT LICENSE',
+      packages=find_packages(),
+      install_requires=[
+      ],)
